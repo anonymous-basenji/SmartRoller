0e0600e0b7664d6fe69ebbbe9358e8db9225b250
anonymous-basenji <andrew.v.sklyarov@gmail.com>
2024-05-29 16:31:12 -0400

    Banner ad on main screen implemented. Getting invalid application ID errors.

--- app/src/main/java/com/nexdev/smartroller/MainActivity.kt
+++ app/src/main/java/com/nexdev/smartroller/MainActivity.kt
@@ -5,10 +5,14 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Vibrator
+import android.util.DisplayMetrics
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
 import com.nexdev.smartroller.adapter.ItemAdapter
 import com.google.android.gms.ads.MobileAds
 import com.google.android.gms.ads.RequestConfiguration.*
@@ -62,6 +66,8 @@ open class MainActivity : AppCompatActivity() {
             .build()
 
         MobileAds.setRequestConfiguration(updatedRequestConfiguration)
+
+        loadBanner() // load the banner ad at the end
     }
 
     private fun launchHistory() {
@@ -70,7 +76,7 @@ open class MainActivity : AppCompatActivity() {
     }
 
     @SuppressLint("ResourceType", "NotifyDataSetChanged")
-    fun outputSinglePair() {
+    private fun outputSinglePair() {
         val vibe: Vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator // create vibrator
         vibe.vibrate(30) // vibrate for 30 ms
 
@@ -88,7 +94,7 @@ open class MainActivity : AppCompatActivity() {
     }
 
     @SuppressLint("ResourceType", "NotifyDataSetChanged")
-    fun outputDoublePair() {
+    private fun outputDoublePair() {
         //vibrates for 30ms
         val vibe: Vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
         vibe.vibrate(30)
@@ -109,7 +115,7 @@ open class MainActivity : AppCompatActivity() {
     }
 
     @SuppressLint("ResourceType", "NotifyDataSetChanged")
-    fun resetStats() {
+    private fun resetStats() {
         //vibrates for 30ms
         val vibe: Vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
         vibe.vibrate(30)
@@ -126,5 +132,33 @@ open class MainActivity : AppCompatActivity() {
         recyclerView.adapter?.notifyItemChanged(1) //notifies RecyclerView stats area changed
 
         RollItemDatasource.clear() // clears roll item datasource for history recyclerview
+    }
+
+    private val adSize: AdSize
+        get() {
+            val display = windowManager.defaultDisplay
+            val outMetrics = DisplayMetrics()
+            display.getMetrics(outMetrics)
+
+            val density = outMetrics.density
+
+            var adWidthPixels = findViewById<AdView>(R.id.banner_ad).width.toFloat()
+            if(adWidthPixels == 0f)
+                adWidthPixels = outMetrics.widthPixels.toFloat()
+
+            val adWidth = (adWidthPixels / density).toInt()
+            return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth)
+        }
+    private fun loadBanner() {
+        // Create ad view
+        val adView = findViewById<AdView>(R.id.banner_ad)
+        adView.setAdSize(adSize)
+        adView.adUnitId = "ca-app-pub-3940256099942544/9214589741"
+
+        // Create ad request
+        val adRequest = AdRequest.Builder().build()
+
+        // Start loading Ad in background
+        adView.loadAd(adRequest)
     }
 }
